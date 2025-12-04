2a80dc2346ccc6d84174c363417ae52f89914cd3
Vladimir Ignatov <vlad.ignatov@gmail.com>
2025-12-04 15:27:03 -0500

    Auth for the example submission

--- src/index.ts
+++ src/index.ts
@@ -9,13 +9,19 @@ const app = createApp();
 
 // In DEV, create a default submission for convenience
 if (NODE_ENV !== 'production') {
-    const defaultSubmission = new Submission({ destinationBaseUrl: 'http://localhost:3333', name: 'Example Bulk Submission' });
+    const defaultSubmission = new Submission({
+      destinationBaseUrl: 'http://localhost:3333',
+      name: 'Example Bulk Submission',
+      clientId: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqd2tzX3VybCI6Imh0dHA6Ly9sb2NhbGhvc3Q6MzAwMC9rZXlzIiwiaWF0IjoxNzY0ODYzMjEzfQ.hItKTj4j1YZw5SHBS-qMsXVgUbQBAut05Z7jD_LApr8',
+      authType: 'bearer',
+      tokenUrl: 'http://localhost:3333/token'
+    });
     (defaultSubmission as any).id = 'example-submission';
-    defaultSubmission.addJob({ manifestUrl: `${BASE_URL}/api/manifests/1`, FHIRBaseUrl: BASE_URL });
-    defaultSubmission.addJob({ manifestUrl: `${BASE_URL}/api/manifests/2`, FHIRBaseUrl: BASE_URL });
-    defaultSubmission.addJob({ manifestUrl: `${BASE_URL}/api/manifests/3`, FHIRBaseUrl: BASE_URL });
-    defaultSubmission.addJob({ manifestUrl: `${BASE_URL}/api/manifests/4`, FHIRBaseUrl: BASE_URL });
-    defaultSubmission.addJob({ manifestUrl: `${BASE_URL}/api/manifests/5`, FHIRBaseUrl: BASE_URL });
+    defaultSubmission.addJob({ manifestUrl: `${BASE_URL}/api/manifests/1`, fhirBaseUrl: BASE_URL });
+    defaultSubmission.addJob({ manifestUrl: `${BASE_URL}/api/manifests/2`, fhirBaseUrl: BASE_URL });
+    defaultSubmission.addJob({ manifestUrl: `${BASE_URL}/api/manifests/3`, fhirBaseUrl: BASE_URL });
+    defaultSubmission.addJob({ manifestUrl: `${BASE_URL}/api/manifests/4`, fhirBaseUrl: BASE_URL });
+    defaultSubmission.addJob({ manifestUrl: `${BASE_URL}/api/manifests/5`, fhirBaseUrl: BASE_URL });
     db.sessions.set(defaultSubmission.id, defaultSubmission);
 }
 
